55add25af43afceb9de56138dd4b31be791cb934
Thuc Tung Nguyen <38500409+thuctung@users.noreply.github.com>
2026-07-31 14:05:37 +0700

    Update reset password (#8)

--- src/app/api/auth/forgot-password/route.ts
+++ src/app/api/auth/forgot-password/route.ts
@@ -1,16 +1,18 @@
 import { NextResponse } from "next/server";
 
 import { supabaseAdmin } from "@/lib/supabase/server";
+import { APP_URL } from "@/commons/constant";
 
 export async function POST(request: Request) {
-    const {
-        email
-    } = await request.json();
-    const{error} :any= supabaseAdmin.auth.resetPasswordForEmail(email, );
+  const { email } = await request.json();
+  const { data, error }: any = await supabaseAdmin.auth.resetPasswordForEmail(email, {
+    redirectTo: `${APP_URL}/update-password`,
+  });
+  console.log("data", data);
+  console.log("error", error);
+  if (error) {
+    return NextResponse.json(data, { status: 500 });
+  }
 
-    if (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-
-    return NextResponse.json({ data: {} }, { status: 200 });
+  return NextResponse.json({ data: {} }, { status: 200 });
 }
